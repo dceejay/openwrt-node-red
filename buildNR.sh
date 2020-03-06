34a9b473d4b6f54dae44e5c385e4d9b4226a0c4e
robanderson <robanderson@me.com>
2020-03-06 20:40:19 +1300

    tweaks to install script

--- buildNR.sh
+++ buildNR.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
+###
+###  There are some custom commands here to get things working on my system. 
+###  Please check to see if they are appropiate for you before using
+###
 cd /root
 #opkg list-upgradable | cut -f 1 -d ' ' | xargs opkg upgrade
 opkg install node-npm
 npm i -g npm
 npm i -g --unsafe-perm node-red
 mkdir -p /etc/node-red
-cp node-red /etc/init.d/node-red
-cp flows.json /etc/node-red/flows.json
-cp flows_cred.json /etc/node-red/flows_cred.json
+cp /usr/src/openwrt-node-red/node-red /etc/init.d/node-red
+cp /usr/src/openwrt-node-red/flows.json /etc/node-red/flows.json
+cp /usr/src/openwrt-node-red/flows_cred.json /etc/node-red/flows_cred.json
+cp -R /usr/src/openwrt-node-red/luci/* /usr/lib/lua/luci/
 cd /etc/node-red
-npm i node-red-node-random node-red-contrib-web-worldmap node-red-dashboard
+npm i node-red-node-random node-red-contrib-web-worldmap node-red-dashboard bufferutil utf-8-validate
 rm -rf /usr/lib/node_modules/node-red/nodes/core/hardware
